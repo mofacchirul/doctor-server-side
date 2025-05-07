95f7e384dd0132d409fd8bdbbe4b099068ea1d80
mofacchirul <mofaccharulhoque@gmail.com>
2025-05-07 20:00:51 +0600

    delet

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASSWORD}@cluster0.98tazfb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -19,10 +20,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
-
-
-
 
 async function run() {
   try {
@@ -34,23 +31,38 @@ async function run() {
       const result = await All_doctor.find().toArray();
       res.send(result);
     });
-    app.get("/alldoctor/:id",async(req,res)=>{
+
+    app.get("/alldoctor/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await All_doctor.findOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-
-    app.post("/Appointment ", async (req, res) => {
+    // ✅ Fixed route
+    app.post("/appointment", async (req, res) => {
       const data = req.body;
       const result = await Appointment_collections.insertOne(data);
       res.send(result);
     });
 
 
+    app.get('/appointment',async(req,res)=>{
+      const email = req.query.email;
+
+      const query= {email:email}
+      const result = await Appointment_collections.find(query).toArray();
+        res.send(result);
+    })
 
 
+
+    app.delete('/appointment/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result =await Appointment_collections.deleteOne(query);
+      res.send(result)
+    })
 
 
 
@@ -60,15 +72,12 @@ async function run() {
     console.error("MongoDB connection failed:", error);
   }
 }
-
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+  res.send('Hello World!');
+});
 
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
